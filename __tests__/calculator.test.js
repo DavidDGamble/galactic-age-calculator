095bc20e2112a2253dc7faef2212264f069a185e
daviddgamble <daviddgamble@gmail.com>
2022-11-14 10:01:23 -0800

    Add venus() to tests w/ bad fail.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -7,9 +7,17 @@ describe('UserInfo', () => {
     userInfo = new UserInfo(35, 'male', false);
   });
 
-  test('should create a userInfo object.', () => {
+  test("should create a userInfo object.", () => {
     expect(userInfo.age).toEqual(35);
     expect(userInfo.gender).toEqual('male');
     expect(userInfo.smoker).toEqual(false);
+  });
+
+  test("should return user's age in mercury years.", () => {
+    expect(userInfo.mercury()).toEqual(145);
+  });
+
+  test("should return user's age in venus years.", () => {
+    expect(user.Info.venus()).toEqual(56);
   });
 });
